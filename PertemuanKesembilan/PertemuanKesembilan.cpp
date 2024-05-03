3922893a80d3325e781a88f8b1d289b40583923e
KamiliaNurmala <kamilia.nurmala.ft23@mail.umy.ac.id>
2024-05-03 17:56:43 +0700

    membuat procedure dengan nama 'traverse'

--- PertemuanKesembilan/PertemuanKesembilan.cpp
+++ PertemuanKesembilan/PertemuanKesembilan.cpp
@@ -114,3 +114,21 @@ void deleteNode() {
 bool listEmpty() {
 	return (START == NULL);
 }
+
+void traverse() {
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else
+	{
+		cout << "\nRecord in descending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+
+		while (currentNode != NULL)
+		{
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
+		}
+	}
+}
